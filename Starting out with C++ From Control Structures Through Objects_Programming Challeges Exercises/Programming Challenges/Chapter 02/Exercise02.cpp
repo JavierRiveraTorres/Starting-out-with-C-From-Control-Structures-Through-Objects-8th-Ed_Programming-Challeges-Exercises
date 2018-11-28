c9da718a02d004a35ac97f0dcdf399f37f6bdd59
KikoRT <30353728+KikoRT@users.noreply.github.com>
2018-11-28 01:02:14 -0400

    Update Exercise02.cpp

--- Starting out with C++ From Control Structures Through Objects_Programming Challeges Exercises/Programming Challenges/Chapter 02/Exercise02.cpp
+++ Starting out with C++ From Control Structures Through Objects_Programming Challeges Exercises/Programming Challenges/Chapter 02/Exercise02.cpp
@@ -1,9 +1,9 @@
 /********************************************************************************************
-* Sales Prediction																			*
-*																							*
-* The East Coast sales division of a company generates 58 percent of total sales.Based		*
-* on that percentage, write a program that will predict how much the East Coast division	*
-* will generate if the company has $8.6 million in sales this year.							*
+* Sales Prediction									    *
+*											    *
+* The East Coast sales division of a company generates 58 percent of total sales.Based	    *
+* on that percentage, write a program that will predict how much the East Coast division    *
+* will generate if the company has $8.6 million in sales this year.			    *
 ********************************************************************************************/
 #include <iostream>
 #include <iomanip>
