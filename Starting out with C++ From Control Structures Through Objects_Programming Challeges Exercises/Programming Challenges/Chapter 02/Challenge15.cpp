52a6fab754462207d028aad9ba73c1d58e7c2028
KikoRT <30353728+KikoRT@users.noreply.github.com>
2018-11-28 17:02:19 -0400

    Update and rename Exercise15.cpp to Challenge15.cpp

--- Starting out with C++ From Control Structures Through Objects_Programming Challeges Exercises/Programming Challenges/Chapter 02/Challenge15.cpp
+++ Starting out with C++ From Control Structures Through Objects_Programming Challeges Exercises/Programming Challenges/Chapter 02/Challenge15.cpp
@@ -1,11 +1,11 @@
 /********************************************************************************************
-* Triangle Pattern:																			*
-* 																							*
-* Write a program that displays the following pattern on the screen:						*
-*    *																						*
-*   ***																						*
-*  *****																					*
-* *******																					*
+* Triangle Pattern:									    *
+* 											    *
+* Write a program that displays the following pattern on the screen:			    *
+*    *											    *
+*   ***											    *
+*  *****										    *
+* *******										    *
 ********************************************************************************************/
 #include <iostream>
 using namespace std;
