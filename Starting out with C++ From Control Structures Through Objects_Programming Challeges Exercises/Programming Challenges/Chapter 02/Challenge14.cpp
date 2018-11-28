f408376099a615116a03b336d5e486a14361c626
KikoRT <30353728+KikoRT@users.noreply.github.com>
2018-11-28 17:01:21 -0400

    Update and rename Exercise14.cpp to Challenge14.cpp

--- Starting out with C++ From Control Structures Through Objects_Programming Challeges Exercises/Programming Challenges/Chapter 02/Challenge14.cpp
+++ Starting out with C++ From Control Structures Through Objects_Programming Challeges Exercises/Programming Challenges/Chapter 02/Challenge14.cpp
@@ -1,15 +1,15 @@
 /********************************************************************************************
-* Personal Information:																		*
-* 																							*
-* Write a program that displays the following pieces of information, each on a separate 	*
-* line:																						*
-* 																							*
-* Your name																					*
-* Your address, with city, state, and ZIP code												*
-* Your telephone number																		*
-* Your college major																		*
-* 																							*
-* Use only a single cout statement to display all of this information.						*
+* Personal Information:									    *
+* 											    *
+* Write a program that displays the following pieces of information, each on a separate     *
+* line:											    *
+* 											    *
+* Your name										    *
+* Your address, with city, state, and ZIP code						    *
+* Your telephone number									    *
+* Your college major									    *
+* 											    *
+* Use only a single cout statement to display all of this information.			    *
 ********************************************************************************************/
 #include <iostream>
 #include <string>
