a9773d35fea545620caebc5b484a13dfc3fec215
KikoRT <30353728+KikoRT@users.noreply.github.com>
2018-11-28 01:09:57 -0400

    Update Exercise09.cpp

--- Starting out with C++ From Control Structures Through Objects_Programming Challeges Exercises/Programming Challenges/Chapter 02/Exercise09.cpp
+++ Starting out with C++ From Control Structures Through Objects_Programming Challeges Exercises/Programming Challenges/Chapter 02/Exercise09.cpp
@@ -1,11 +1,11 @@
 /********************************************************************************************
-* Cyborg Data Type Sizes:																	*
-* 																							*
-* You have been given a job as a programmer on a Cyborg supercomputer. In order to			*
-* accomplish some calculations, you need to know how many bytes the following data			*
-* types use: char , int , float , and double . You do not have any manuals, so you can’t	*
-* look this information up. Write a C++ program that will determine the amount of			*
-* memory used by these types and display the information on the screen.						*
+* Cyborg Data Type Sizes:								    *
+* 											    *
+* You have been given a job as a programmer on a Cyborg supercomputer. In order to	    *
+* accomplish some calculations, you need to know how many bytes the following data	    *
+* types use: char , int , float , and double . You do not have any manuals, so you canâ€™t    *
+* look this information up. Write a C++ program that will determine the amount of	    *
+* memory used by these types and display the information on the screen.			    *
 ********************************************************************************************/
 #include <iostream>
 using namespace std;
