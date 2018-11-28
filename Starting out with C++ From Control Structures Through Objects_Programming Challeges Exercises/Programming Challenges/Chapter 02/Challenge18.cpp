b72817bad6ec1519ec69894f94d1c6ce27335d68
KikoRT <30353728+KikoRT@users.noreply.github.com>
2018-11-28 17:05:55 -0400

    Update and rename Exercise18.cpp to Challenge18.cpp

--- Starting out with C++ From Control Structures Through Objects_Programming Challeges Exercises/Programming Challenges/Chapter 02/Challenge18.cpp
+++ Starting out with C++ From Control Structures Through Objects_Programming Challeges Exercises/Programming Challenges/Chapter 02/Challenge18.cpp
@@ -1,18 +1,18 @@
 /********************************************************************************************
-* Energy Drink Consumption:																	*
-* 																							*
-* A soft drink company recently surveyed 16,500 of its customers and found that				*
-* approximately 15 percent of those surveyed purchase one or more energy drinks per			*
-* week. Of those customers who purchase energy drinks, approximately 58 percent of			*
-* them prefer citrus-flavored energy drinks. 											    *
-*																							*
-* Write a program that displays the following:												*
-* 																							*
-* • The approximate number of customers in the survey who purchase one or more				*
-*   energy drinks per week																	*
-* 																							*
-* • The approximate number of customers in the survey who prefer citrus-flavored energy		*
-*   drinks																					*
+* Energy Drink Consumption:						  		    *
+* 											    *
+* A soft drink company recently surveyed 16,500 of its customers and found that		    *
+* approximately 15 percent of those surveyed purchase one or more energy drinks per	    *
+* week. Of those customers who purchase energy drinks, approximately 58 percent of	    *
+* them prefer citrus-flavored energy drinks. 						    *
+*										            *
+* Write a program that displays the following:					            *
+* 											    *
+* â€¢ The approximate number of customers in the survey who purchase one or more		    *
+*   energy drinks per week								    *
+* 											    *
+* â€¢ The approximate number of customers in the survey who prefer citrus-flavored energy	    *
+*   drinks										    *
 ********************************************************************************************/
 #include <iostream>
 using namespace std;
