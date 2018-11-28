d0e2c398133f085d2246532090ffb108357362ce
KikoRT <30353728+KikoRT@users.noreply.github.com>
2018-11-28 17:00:35 -0400

    Update and rename Exercise13.cpp to Challenge13.cpp

--- Starting out with C++ From Control Structures Through Objects_Programming Challeges Exercises/Programming Challenges/Chapter 02/Challenge13.cpp
+++ Starting out with C++ From Control Structures Through Objects_Programming Challeges Exercises/Programming Challenges/Chapter 02/Challenge13.cpp
@@ -1,9 +1,9 @@
 /********************************************************************************************
-* Circuit Board Price:																		*
-* 																							*
-* An electronics company sells circuit boards at a 35 percent profit. Write a program that	*
-* will calculate the selling price of a circuit board that costs $14.95. Display the result	*
-* on the screen.																			*
+* Circuit Board Price:									    *
+* 											    *
+* An electronics company sells circuit boards at a 35 percent profit. Write a program that  *
+* will calculate the selling price of a circuit board that costs $14.95. Display the result *
+* on the screen.									    *
 ********************************************************************************************/
 #include <iostream>
 #include <iomanip>
