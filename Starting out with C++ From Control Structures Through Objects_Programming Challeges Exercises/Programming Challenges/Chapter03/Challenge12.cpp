46a2927dda051b8923b68b9f373058e70035d60c
KikoRT <30353728+KikoRT@users.noreply.github.com>
2018-12-01 17:37:27 -0400

    Update Challenge12.cpp

--- Starting out with C++ From Control Structures Through Objects_Programming Challeges Exercises/Programming Challenges/Chapter03/Challenge12.cpp
+++ Starting out with C++ From Control Structures Through Objects_Programming Challeges Exercises/Programming Challenges/Chapter03/Challenge12.cpp
@@ -1,11 +1,11 @@
 /**************************************************************************************
-* Celsius to Fahrenheit:															  *
-* 																					  *
-* Write a program that converts Celsius temperatures to Fahrenheit temperatures. 	  *
-* 																					  *
-* The formula is:																	  *
-* 	F = 9/5 * C + 32																  *
-* 	F is the Fahrenheit temperature, and C is the Celsius temperature.				  *
+* Celsius to Fahrenheit:							      *
+* 										      *
+* Write a program that converts Celsius temperatures to Fahrenheit temperatures.      *
+* 										      *
+* The formula is:								      *
+* 	F = 9/5 * C + 32							      *
+* 	F is the Fahrenheit temperature, and C is the Celsius temperature.	      *
 **************************************************************************************/
 #include <iostream>
 using namespace std;
