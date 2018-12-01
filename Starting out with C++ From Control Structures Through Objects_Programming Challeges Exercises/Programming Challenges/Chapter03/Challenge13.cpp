b12c8f0f847d5f42a1a9606d4703b8bec3a72f99
KikoRT <30353728+KikoRT@users.noreply.github.com>
2018-12-01 17:39:25 -0400

    Update Challenge13.cpp

--- Starting out with C++ From Control Structures Through Objects_Programming Challeges Exercises/Programming Challenges/Chapter03/Challenge13.cpp
+++ Starting out with C++ From Control Structures Through Objects_Programming Challeges Exercises/Programming Challenges/Chapter03/Challenge13.cpp
@@ -1,17 +1,17 @@
 /**************************************************************************************
-* Currency:																			  *
-* 																					  *
-* Write a program that will convert U.S. dollar amounts to Japanese yen and 		  *
-* to euros, storing the conversion factors in the constants YEN_PER_DOLLAR and 		  *
-* EUROS_PER_DOLLAR . To get the most up-to-date exchange rates, search the 			  *
-* Internet using the term ìcurrency exchange rateî. 								  *
-* 																					  *
-* If you cannot find the most recent exchange rates, use the following:				  *
-* 	 1 Dollar = 98.93 Yen															  *
-* 	 1 Dollar = 0.74 Euros															  *
-* 																					  *
-* Format your currency amounts in fixed-point notation, with two decimal 			  *
-* places of precision, and be sure the decimal point is always displayed.			  *
+* Currency:									      *
+* 										      *
+* Write a program that will convert U.S. dollar amounts to Japanese yen and 	      *
+* to euros, storing the conversion factors in the constants YEN_PER_DOLLAR and 	      *
+* EUROS_PER_DOLLAR . To get the most up-to-date exchange rates, search the            *
+* Internet using the term ‚Äúcurrency exchange rate‚Äù. 			              *
+* 										      *
+* If you cannot find the most recent exchange rates, use the following:		      *
+* 	 1 Dollar = 98.93 Yen							      *
+* 	 1 Dollar = 0.74 Euros						              *
+* 										      *
+* Format your currency amounts in fixed-point notation, with two decimal 	      *
+* places of precision, and be sure the decimal point is always displayed.	      *
 **************************************************************************************/
 #include <iostream>
 #include <iomanip>
