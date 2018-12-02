706f59bc0edb6654809e1bc894d42e2c8cc55843
KikoRT <30353728+KikoRT@users.noreply.github.com>
2018-12-02 10:24:47 -0400

    Update Challenge17b.cpp

--- Starting out with C++ From Control Structures Through Objects_Programming Challeges Exercises/Programming Challenges/Chapter03/Challenge17b.cpp
+++ Starting out with C++ From Control Structures Through Objects_Programming Challeges Exercises/Programming Challenges/Chapter03/Challenge17b.cpp
@@ -1,15 +1,15 @@
 /**************************************************************************************
-* Math Tutor:	Alternate version using some functions								  *
-* 																					  *
-* Write a program that can be used as a math tutor for a young student. 			  *
-* 																					  *
-* The program should display two random numbers to be added, such as				  *
-* 	 247 + 129																		  *
-* 																					  *
-* The program should then pause while the student works on the problem. 			  *
-* When the student is ready to check the answer, he or she can press a key and 		  *
-* the program will display the correct solution:									  *
-* 	 247 + 129 = 376																  *
+* Math Tutor:	Alternate version using some functions				      *
+* 										      *
+* Write a program that can be used as a math tutor for a young student. 	      *
+* 										      *
+* The program should display two random numbers to be added, such as		      *
+* 	 247 + 129								      *
+* 										      *
+* The program should then pause while the student works on the problem. 	      *
+* When the student is ready to check the answer, he or she can press a key and 	      *
+* the program will display the correct solution:				      *
+* 	 247 + 129 = 376							      *
 **************************************************************************************/
 #include <iostream>
 #include <iomanip>
