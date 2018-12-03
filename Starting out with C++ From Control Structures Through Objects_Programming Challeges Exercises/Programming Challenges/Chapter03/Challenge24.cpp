e419ae2112b42cea64b935fd260435ea8da5d4f4
JavierRiveraTorres <30353728+JavierRiveraTorres@users.noreply.github.com>
2018-12-02 23:58:13 -0400

    Update Challenge24.cpp

--- Starting out with C++ From Control Structures Through Objects_Programming Challeges Exercises/Programming Challenges/Chapter03/Challenge24.cpp
+++ Starting out with C++ From Control Structures Through Objects_Programming Challeges Exercises/Programming Challenges/Chapter03/Challenge24.cpp
@@ -1,24 +1,24 @@
 /******************************************************************************
-* Word Game:																  *
-* 																			  *
-* Write a program that plays a word game with the user. The program should 	  *
-* ask the user to enter the following:										  *
-* 																			  *
-* • His or her name															  *
-* • His or her age															  *
-* • The name of a city														  *
-* • The name of a college													  *
-* • A profession															  *
-* • A type of animal														  *
-* • A pet’s name															  *
-* 																			  *
-* After the user has entered these items, the program should display the 	  *
-* following story, inserting the user’s input into the appropriate locations: *
-* 																			  *
-* There once was a person named NAME who lived in CITY . At the age of		  *
-* AGE , NAME went to college at COLLEGE . NAME graduated and went to work	  *
-* as a PROFESSION . Then, NAME adopted a(n) ANIMAL named PETNAME . They		  *
-* both lived happily ever after!											  *
+* Word Game:								      *
+* 									      *
+* Write a program that plays a word game with the user. The program should    *
+* ask the user to enter the following:					      *
+* 								              *
+* â€¢ His or her name							      *
+* â€¢ His or her age							      *
+* â€¢ The name of a city							      *
+* â€¢ The name of a college						      *
+* â€¢ A profession							      *
+* â€¢ A type of animal							      *
+* â€¢ A petâ€™s name							      *
+* 									      *
+* After the user has entered these items, the program should display the      *
+* following story, inserting the userâ€™s input into the appropriate locations: *
+* 									      *
+* There once was a person named NAME who lived in CITY . At the age of	      *
+* AGE , NAME went to college at COLLEGE . NAME graduated and went to work     *
+* as a PROFESSION . Then, NAME adopted a(n) ANIMAL named PETNAME . They	      *
+* both lived happily ever after!				              *
 ******************************************************************************/
 #include <iostream>
 #include <string>
