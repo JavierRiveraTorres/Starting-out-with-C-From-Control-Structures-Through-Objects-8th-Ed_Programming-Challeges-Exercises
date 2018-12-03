3ccbb9363adc95293ae232aa00ff2e45b88903b6
JavierRiveraTorres <30353728+JavierRiveraTorres@users.noreply.github.com>
2018-12-02 23:49:21 -0400

    Update Challenge20.cpp

--- Starting out with C++ From Control Structures Through Objects_Programming Challeges Exercises/Programming Challenges/Chapter03/Challenge20.cpp
+++ Starting out with C++ From Control Structures Through Objects_Programming Challeges Exercises/Programming Challenges/Chapter03/Challenge20.cpp
@@ -1,26 +1,26 @@
 /****************************************************************************************
-* Pizza Pi:																				*
-*																						*
-* Joe’s Pizza Palace needs a program to calculate the number of slices a pizza of		*
-* any size	can be divided into. The program should perform the following steps:		*
-*																						*
-* A) Ask the user for the diameter of the pizza in inches.								*
-* B) Calculate the number of slices that may be taken from a pizza of that size.		*
-* C) Display a message telling the number of slices.									*
-*																						*
-* To calculate the number of slices that may be taken from the pizza, you must know		*
-* the following facts:																	*
-* • Each slice should have an area of 14.125 inches.									*
-* • To calculate the number of slices, simply divide the area of the pizza by 14.125.	*
-* • The area of the pizza is calculated with this formula:								*
-*																						*
-* Area = PI * r^2																		*
-*																						*
-* Note: PI is the Greek letter pi. 3.14159 can be used as its value. The variable r		*
-* is the radius of the pizza. Divide the diameter by 2 to get the radius.				*
-*																						*
-* Make sure the output of the program displays the number of slices in fixed point		*
-* notation,	rounded to one decimal place of precision. Use a named constant for pi.		*
+* Pizza Pi:										*
+*											*
+* Joeâ€™s Pizza Palace needs a program to calculate the number of slices a pizza of	*
+* any size	can be divided into. The program should perform the following steps:	*
+*											*
+* A) Ask the user for the diameter of the pizza in inches.				*
+* B) Calculate the number of slices that may be taken from a pizza of that size.	*
+* C) Display a message telling the number of slices.					*
+*											*
+* To calculate the number of slices that may be taken from the pizza, you must know	*
+* the following facts:									*
+* â€¢ Each slice should have an area of 14.125 inches.					*
+* â€¢ To calculate the number of slices, simply divide the area of the pizza by 14.125.	*
+* â€¢ The area of the pizza is calculated with this formula:				*
+*											*
+* Area = PI * r^2									*
+*											*
+* Note: PI is the Greek letter pi. 3.14159 can be used as its value. The variable r	*
+* is the radius of the pizza. Divide the diameter by 2 to get the radius.		*
+*											*
+* Make sure the output of the program displays the number of slices in fixed point	*
+* notation,	rounded to one decimal place of precision. Use a named constant for pi.	*
 ****************************************************************************************/
 #include <iostream>
 #include <iomanip>
